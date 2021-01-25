9bac9082c48b8b599710fa7abfd7c08e34adbd2c
bostdiek <bostdiek@gmail.com>
2021-01-25 09:52:17 -0500

    Analyze for unlabeled set

--- src/scripts/launch_analyzepython_slurm.py
+++ src/scripts/launch_analyzepython_slurm.py
@@ -3,14 +3,14 @@ import numpy as np
 import time
 import os
 
-channels = ['chan3']
+channels = ['chan1', 'chan2a', 'chan2b', 'chan3']
 
 for c in channels:
     files = os.listdir(f'/n/home02/bostdiek/DarkMachines/model/logs/{c}/')
     print(files)
 
     for f in files:
-        if '2020-10-30-' not in f:
+        if 'VariationalAutoEncoderBeta-1.0-8-256' not in f:
             continue
         fname = f'/n/home02/bostdiek/DarkMachines/src/scripts/bash_slurm/{c}_{f[:-4]}.sh'
         print(fname)
@@ -19,7 +19,7 @@ for c in channels:
             fout.write(f"#SBATCH --job-name={c}_{f[:-4]}\n")
             fout.write("#SBATCH -n 4               # Number of cores\n")
             fout.write("#SBATCH -N 1                # Ensure that all cores are on one machine\n")
-            fout.write("#SBATCH -t 0-08:00          # Runtime in D-HH:MM, minimum of 10 minutes\n")
+            fout.write("#SBATCH -t 0-04:00          # Runtime in D-HH:MM, minimum of 10 minutes\n")
             fout.write("#SBATCH -p fas_gpu           # Partition to submit to\n")
             fout.write("#SBATCH --gres=gpu:1           # Partition to submit to\n")
             fout.write("#SBATCH --mem=63000         # Memory pool for all cores (see also --mem-per-cpu)\n")
